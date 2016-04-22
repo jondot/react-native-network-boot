b3d058cc6eb1df77ca94fa0afbb78a01f01561e3
Dotan Nahum <jondotan@gmail.com>
2016-04-22 19:53:14 +0300

    take only first address

--- boot.sh
+++ boot.sh
@@ -2,7 +2,7 @@
 # todo: improve this, some boxes has en5, en1, etc
 
 # use ifconfig, remove loopack and virtualbox IPs
-rnhost=`ifconfig | sed -En 's/127.0.0.1//;s/192.168.99.*//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`
+rnhost=`ifconfig | sed -En 's/127.0.0.1//;s/192.168.99.*//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | head -n1`
 plist=${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/../${INFOPLIST_PATH}
 
 /usr/libexec/PlistBuddy -c "Add :RNHost string" $plist

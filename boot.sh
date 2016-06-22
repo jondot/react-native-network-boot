3918ff78bfd97b5e04b35378ecd74d8537cc4294
Dotan Nahum <jondotan@gmail.com>
2016-06-22 14:10:09 +0300

    only fuss with info.plist in debug-like configurations

--- boot.sh
+++ boot.sh
@@ -5,6 +5,18 @@
 rnhost=`ifconfig | sed -En 's/127.0.0.1//;s/192.168.99.*//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | head -n1`
 plist=${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/../${INFOPLIST_PATH}
 
+case "$CONFIGURATION" in
+  Debug)
+    ;;
+  "")
+    echo "$0 must be invoked by Xcode"
+    exit 1
+    ;;
+  *)
+    echo "$0 will not work in release configurations - configure by hand"
+    exit 1
+    ;;
+esac
 
 echo Setting ATS exception for arbitrary loads
 /usr/libexec/PlistBuddy -c "Delete :NSAppTransportSecurity" $plist &>/dev/null

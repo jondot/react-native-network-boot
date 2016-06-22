7cca58f7b69602093130b3bb8b692ba7bfff42ba
Dotan Nahum <jondotan@gmail.com>
2016-06-22 14:04:25 +0300

    support RN 0.28, see more here https://github.com/facebook/react-native/commit/0f57702cd813cab7a68f66c583b5a5c934d579d5

--- boot.sh
+++ boot.sh
@@ -5,7 +5,12 @@
 rnhost=`ifconfig | sed -En 's/127.0.0.1//;s/192.168.99.*//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | head -n1`
 plist=${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/../${INFOPLIST_PATH}
 
-/usr/libexec/PlistBuddy -c "Add :RNHost string" $plist
+
+echo Setting ATS exception for arbitrary loads
+/usr/libexec/PlistBuddy -c "Delete :NSAppTransportSecurity" $plist &>/dev/null
+/usr/libexec/PlistBuddy -c "Set :NSAppTransportSecurity:NSAllowsArbitraryLoads bool true" $plist
+
 echo Setting React Native RNHost variable to $rnhost
+/usr/libexec/PlistBuddy -c "Add :RNHost string" $plist
 /usr/libexec/PlistBuddy -c "Set :RNHost ${rnhost}" $plist
 echo Done.

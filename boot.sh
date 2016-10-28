ec887c1bd584e81a9efa62cb994a95835d99cf7c
Keith Grennan <keith@nearlyfree.org>
2016-10-28 10:56:52 -0700

    Exclude OpenVPN loopback addresses

--- boot.sh
+++ boot.sh
@@ -2,7 +2,7 @@
 # todo: improve this, some boxes has en5, en1, etc
 
 # use ifconfig, remove loopack and virtualbox IPs
-rnhost=`ifconfig | sed -En 's/127.0.0.1//;s/192.168.99.*//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | head -n1`
+rnhost=`ifconfig | sed -En 's/127.0.0.1//;s/127.94.0.(1|2)//;s/192.168.99.*//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | head -n1`
 plist=${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/../${INFOPLIST_PATH}
 
 case "$CONFIGURATION" in
